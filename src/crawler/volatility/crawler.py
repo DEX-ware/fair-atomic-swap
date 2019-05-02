a327f2006f7e837051dc974830a34c8111ff894f
HAOYUatHZ <haoyu@protonmail.com>
2019-05-02 18:40:34 +0800

    feat: init logging

--- src/crawler/volatility/crawler.py
+++ src/crawler/volatility/crawler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from datetime import datetime
 import requests
 import settings
 
@@ -11,10 +12,17 @@ def get_data(coin):
 
 def main():
     for coin in settings.coins:
+        print("Getting data of %s..." % coin)
         data = get_data(coin)
-        print(coin)
-        print(data["prices"])
-        print()
+        print("Saving data of %s..." % coin)
+        file = open('{}-{}.csv'.format(coin, datetime.today().strftime('%Y-%m-%d')), "w")
+        try:
+            file.write("")
+            file.flush()
+            file.close()
+        except Exception as e:
+            print("Error:", e)
+    print("Done!")
 
 if __name__ == '__main__':
     main()
