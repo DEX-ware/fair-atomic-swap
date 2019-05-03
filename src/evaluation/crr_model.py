7a53cd78e8d00cb59591674983aaff86915a4a18
Runchao Han <elvisage941102@gmail.com>
2019-05-03 16:58:25 +1000

    comment crr model

--- src/evaluation/crr_model.py
+++ src/evaluation/crr_model.py
@@ -2,57 +2,74 @@ import numpy as np
 import math as m
 
 
-def CRR(n, S, K, r, sigma_a, T, PC):
+def CRR(n, S, K, r, sigma_a, T, contract_type):
     '''
-    n: steps of the binomial tree
-    S: price
-    K: strike price
-    r: risk factor
-    sigma_a: annualized volatility
-    T: expiration time
-    PC: type of the contract (0 for call, 1 for put)
+    The Cox-Ross-Rubinstein Model
+    Please refer to https://en.wikipedia.org/wiki/Binomial_options_pricing_model
+    Inputs:
+        n: steps of the binomial tree
+        S: price
+        K: strike price
+        r: risk factor
+        sigma_a: annualized volatility
+        T: strike time
+        contract_type: type of the contract (0 for call, 1 for put)
+    Returns:
+        S_tree: The binomial tree of the asset prices
+        C_tree: The binomial tree of the option prices
     '''
+
+    # Step 1: Create the binomial price tree
     dt = T/n
     u = m.exp(sigma_a * m.sqrt(dt))
     d = 1/u
     p = (m.exp(r*dt)-d)/(u-d)
-    Pm = np.zeros((n+1, n+1))
-    Cm = np.zeros((n+1, n+1))
+    S_tree = np.zeros((n+1, n+1))
+    C_tree = np.zeros((n+1, n+1))
     tmp = np.zeros((2, n+1))
     for j in range(n+1):
-        tmp[0, j] = S*m.pow(d, j)
-        tmp[1, j] = S*m.pow(u, j)
+        tmp[0, j] = S * m.pow(d, j)
+        tmp[1, j] = S * m.pow(u, j)
     tot = np.unique(tmp)
     c = n
+
+    # Step 1.5: Find the asset price for each node of the binomial price tree
     for i in range(c+1):
         for j in range(c+1):
-            Pm[i, j-c-1] = tot[(n-i)+j]
+            S_tree[i, j-c-1] = tot[(n-i)+j]
         c = c-1
+
+    # Step 2+3: Find the option value of each node
+    # starting from each final node
     for j in range(n+1, 0, -1):
         for i in range(j):
-            if (PC == 1):
+            if (contract_type == 1):
                 if(j == n+1):
-                    Cm[i, j-1] = max(K-Pm[i, j-1], 0)
+                    C_tree[i, j-1] = max(K-S_tree[i, j-1], 0)
                 else:
-                    Cm[i, j-1] = m.exp(-.05*dt) * \
-                        (p*Cm[i, j] + (1-p)*Cm[i+1, j])
-            if (PC == 0):
+                    C_tree[i, j-1] = m.exp(-.05*dt) * \
+                        (p*C_tree[i, j] + (1-p)*C_tree[i+1, j])
+            if (contract_type == 0):
                 if (j == n + 1):
-                    Cm[i, j-1] = max(Pm[i, j-1]-K, 0)
+                    C_tree[i, j-1] = max(S_tree[i, j-1]-K, 0)
                 else:
-                    Cm[i, j-1] = m.exp(-.05*dt) * \
-                        (p*Cm[i, j] + (1-p)*Cm[i+1, j])
-    return [Pm, Cm]
+                    C_tree[i, j-1] = m.exp(-.05*dt) * \
+                        (p*C_tree[i, j] + (1-p)*C_tree[i+1, j])
+
+    return S_tree, C_tree
 
 
 if __name__ == '__main__':
+    # input
     S = 100.0
     K = 100.0
     r = 0.05
     v = 0.3
     T = 20.0/36
     n = 17
-    PC = 0
-    Pm, Cm = CRR(n, S, K, r, v, T, PC)
-    print('Pricing:\n', np.matrix(Pm.astype(int)))
-    print('Option Values:\n', np.matrix(Cm.astype(int)))
+    contract_type = 0
+
+    # get all asset prices S_tree and option prices C_tree
+    S_tree, C_tree = CRR(n, S, K, r, v, T, contract_type)
+    print('Asset Prices:\n', np.matrix(S_tree.astype(int)))
+    print('Option Prices:\n', np.matrix(C_tree.astype(int)))
