4e798ee6565cea1f5b2205de3d4f1748161d7975
HAOYUatHZ <haoyu@protonmail.com>
2019-09-08 14:33:04 +0800

    wip

--- src/atomicswap/eip2266/ERCTokenImplementation.sol
+++ src/atomicswap/eip2266/ERCTokenImplementation.sol
@@ -33,25 +33,26 @@ contract ERC2266
 
     mapping(bytes32 => Swap) public swaps;
 
-    event AssetRefunded(
-        uint256 refundTimestamp,
-        bytes32 secretHash,
-        address refunder,
-        uint256 value
-    );
+    // event AssetRefunded(
+    //     uint256 refundTimestamp,
+    //     bytes32 secretHash,
+    //     address refunder,
+    //     uint256 value
+    // );
 
-    event AssetRedeemed(
-        uint256 redeemTimestamp,
-        bytes32 secretHash,
-        bytes32 secret,
-        address redeemer,
-        uint256 value
-    );
+    // event AssetRedeemed(
+    //     uint256 redeemTimestamp,
+    //     bytes32 secretHash,
+    //     bytes32 secret,
+    //     address redeemer,
+    //     uint256 value
+    // );
 
     event PremiumRefunded(
         uint256 refundTimestamp,
         bytes32 secretHash,
         address refunder,
+        address token,
         uint256 value
     );
 
@@ -59,57 +60,58 @@ contract ERC2266
         uint256 redeemTimestamp,
         bytes32 secretHash,
         address redeemer,
+        address token,
         uint256 value
     );
 
-    event PremiumFilled(
-        uint256 fillPremiumTimestamp,
-        bytes32 secretHash,
-        address initiator,
-        address participant,
-        uint256 assetValue,
-        uint256 assetRefundTimestamp,
-        uint256 premiumValue,
-        uint256 premiumRefundTimestamp
-    );
+    // event PremiumFilled(
+    //     uint256 fillPremiumTimestamp,
+    //     bytes32 secretHash,
+    //     address initiator,
+    //     address participant,
+    //     uint256 assetValue,
+    //     uint256 assetRefundTimestamp,
+    //     uint256 premiumValue,
+    //     uint256 premiumRefundTimestamp
+    // );
 
-    event Participated(
-        uint256 participateTimestamp,
-        bytes32 secretHash,
-        address initiator,
-        address participant,
-        uint256 assetValue,
-        uint256 assetRefundTimestamp,
-        uint256 premiumValue,
-        uint256 premiumRefundTimestamp
-    );
+    // event Participated(
+    //     uint256 participateTimestamp,
+    //     bytes32 secretHash,
+    //     address initiator,
+    //     address participant,
+    //     uint256 assetValue,
+    //     uint256 assetRefundTimestamp,
+    //     uint256 premiumValue,
+    //     uint256 premiumRefundTimestamp
+    // );
 
-    event Initiated(
-        uint256 initiateTimestamp,
-        bytes32 secretHash,
-        address initiator,
-        address participant,
-        uint256 assetValue,
-        uint256 assetRefundTimestamp,
-        uint256 premiumValue,
-        uint256 premiumRefundTimestamp
-    );
+    // event Initiated(
+    //     uint256 initiateTimestamp,
+    //     bytes32 secretHash,
+    //     address initiator,
+    //     address participant,
+    //     uint256 assetValue,
+    //     uint256 assetRefundTimestamp,
+    //     uint256 premiumValue,
+    //     uint256 premiumRefundTimestamp
+    // );
 
-    event SetUpByParticipant(
-        bytes32 secretHash,
-        address initiator,
-        address participant,
-        uint256 assetValue,
-        uint256 premiumValue
-    );
+    // event SetUpByParticipant(
+    //     bytes32 secretHash,
+    //     address initiator,
+    //     address participant,
+    //     uint256 assetValue,
+    //     uint256 premiumValue
+    // );
 
-    event SetUpByInitiator(
-        bytes32 secretHash,
-        address initiator,
-        address participant,
-        uint256 assetValue,
-        uint256 premiumValue
-    );
+    // event SetUpByInitiator(
+    //     bytes32 secretHash,
+    //     address initiator,
+    //     address participant,
+    //     uint256 assetValue,
+    //     uint256 premiumValue
+    // );
 
     constructor() public {}
 }
